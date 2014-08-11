53a2ab84369c6a65774b96189764c543b51ac9c7
Joe Fitzgerald <jfitzgerald@gopivotal.com>
2014-08-11 10:02:12 -0600

    Rename CF To CloudFoundry

--- cloudfoundry.go
+++ cloudfoundry.go
@@ -1,7 +1,7 @@
 package buildpack
 
-// CF is a contract that satisfies the Cloud Foundry buildpack API.
-type CF interface {
+// CloudFoundry is a contract that satisfies the Cloud Foundry buildpack API.
+type CloudFoundry interface {
 	Detect(buildDir string) int
 	Compile(buildDir string, cacheDir string) int
 	Release(buildDir string)
